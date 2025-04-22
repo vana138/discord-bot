063b6dd8b078e1b4d80aaae04ddbffaabf1dbfb6
vana138 <141772135+vana138@users.noreply.github.com>
2025-04-22 23:33:16 +0300

    Update JamBot.py

--- JamBot.py
+++ JamBot.py
@@ -1,34 +1,22 @@
 import os
-import subprocess
-import discord
 from discord.ext import commands
-
-
-
-intents = discord.Intents.default()
-intents.messages = True
-intents.guilds = True
-intents.voice_states = True
-intents.message_content = True
+import discord
 
 class JamBot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix="!", intents=intents, application_id="1330922461973450813")
-
     async def setup_hook(self):
         from commands import setup
         await setup(self)
+        print(f"Бот {self.user} готов к работе!")
+        await self.tree.sync()
+        print("Синхронизировано", len(self.tree.get_commands()), "команд:")
+        for command in self.tree.get_commands():
+            print(f"{command.name} (ID: {command.id})")
 
     async def on_ready(self):
-        print(f'Бот {self.user} готов к работе!')
-        try:
-            synced = await self.tree.sync()
-            # Форматируем вывод команд в столбик
-            commands_list = "\n".join([f"{cmd.name} (ID: {cmd.id})" for cmd in synced])
-            print(f"Синхронизировано {len(synced)} команд:\n{commands_list}")
-        except Exception as e:
-            print(f"Ошибка синхронизации команд: {e}")
-            
-TOKEN = os.getenv("MTMzMDkyMjQ2MTk3MzQ1MDgxMw.GwDDOR.VxwMSg8G2F54kRNNR2wxXVOQ0vMv6NTC3KN7_g")
-bot = JamBot()
+        print(f"Бот {self.user} подключён к Discord!")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = JamBot(command_prefix='/', intents=intents)
+TOKEN = os.getenv("DISCORD_TOKEN")  # Читаем токен из переменной окружения
 bot.run(TOKEN)
